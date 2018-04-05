977f1670da6fd7d1f580607e0e8d07f4aa5bae1f
Benjamin Shanks <benjamin.shanks@gmail.com>
2018-04-04 22:37:19 -0400

    playing with zeros

--- example/electronics/decay.py
+++ example/electronics/decay.py
@@ -17,15 +17,12 @@ imp_avg = -2
 imp_grad = 1.2
 det.siggenInst.SetImpurityAvg(imp_avg, imp_grad)
 
-
-
-
-
 def main():
-    plot_wfs(det)
+    # poles(det)
+    zeros(det)
 
 
-def plot_wfs(det):
+def poles(det):
     det.lp_order=2
 
     det.lp_num = [1,2,1]
@@ -48,16 +45,21 @@ def plot_wfs(det):
 
     cmap = cm.get_cmap('viridis')
 
-    phis = np.logspace(-15, -8, 100, base=np.pi)
+    phis = np.logspace(-20, -8, 100, base=np.pi)
+    mags = 1 - np.logspace(-6, -5, 100, base=10)
     # phis = np.pi - phis
 
     w,h = get_freq_resp(mag, phi)
     ax[0,1].loglog( w, h, color="r")
 
-    for phi2 in phis:
-        color = cmap( (phi2 - phis[0])/(phis[-1] - phis[0]) )
+    # for phi2 in phis:
+        # color = cmap( (phi2 - phis[0])/(phis[-1] - phis[0]) )
+        # mag2=mag
+    for mag2 in mags:
+        phi2=phi
+        color = cmap( (mag2 - mags[0])/(mags[-1] - mags[0]) )
 
-        det.hp_den = em.zpk_to_ba(mag, phi2)
+        det.hp_den = em.zpk_to_ba(mag2, phi2)
         wf_proc = np.copy(det.MakeSimWaveform(25, 0, 25, 1, 125, 0.95, 1000, smoothing=20))
 
         try:
@@ -73,13 +75,73 @@ def plot_wfs(det):
     # plt.legend()
     plt.show()
 
+def zeros(det):
+    det.lp_order=2
 
-def get_freq_resp(p_mag, p_phi):
+    det.lp_num = [1,2,1]
+    det.lp_den = em.zpk_to_ba(0.975, 0.007)
+
+    det.hp_order = 2
+
+
+    mag = 1.-10.**-5.145
+    phi = np.pi**-13.3
+    det.hp_num = [1,-2,1]
+    det.hp_den = em.zpk_to_ba(mag, phi)
+
+    wf_proc = np.copy(det.MakeSimWaveform(25, 0, 25, 1, 125, 0.95, 1000, smoothing=20))
+    wf_compare = np.copy(wf_proc)
+
+    f, ax = plt.subplots(2,2,figsize=(15,8))
+    ax[0,0].plot (wf_compare,  color="r")
+
+    cmap = cm.get_cmap('viridis')
+
+    phis = np.logspace(-20, -8, 100, base=np.pi)
+    mags = 1-np.logspace(-7, 7, 100, base=10)
+    # phis = np.pi - phis
+
+    w,h = get_freq_resp(mag, phi)
+    ax[0,1].loglog( w, h, color="r")
+
+    # for phi2 in phis:
+        # color = cmap( (phi2 - phis[0])/(phis[-1] - phis[0]) )
+        # mag2=mag
+    for mag2 in mags:
+        phi2= np.pi**-13
+
+        color = cmap( (mag2 - mags[0])/(mags[-1] - mags[0]) )
+
+        det.hp_num = em.zpk_to_ba(mag2, phi2)
+        det.hp_num /= np.sum(det.hp_num)/np.sum(det.hp_den)
+
+        wf_proc = np.copy(det.MakeSimWaveform(25, 0, 25, 1, 125, 0.95, 1000, smoothing=20))
+
+        w,h2 = get_freq_resp(mag, phi, mag2, phi2)
+        ax[0,1].loglog( w, h2, color=color)
+
+        try:
+            ax[0,0].plot (wf_proc, label="{}, {}".format(phi,mag), color=color)
+            ax[1,0].plot (wf_proc-wf_compare, label="{}, {}".format(phi,mag), color=color)
+        except TypeError:
+            continue
+
+
+    # plt.legend()
+    plt.show()
+
+
+def get_freq_resp(p_mag, p_phi, z_mag=None, z_phi=None):
     freq_samp = 1E9
     nyq_freq = 0.5*freq_samp
 
-    num = np.array([1,-2,1])
     den1 = em.zpk_to_ba(p_mag, p_phi)
+
+    if z_mag is None:
+        num = np.array([1,-2,1])
+    else:
+        num = em.zpk_to_ba(z_mag, z_phi)
+        num /= np.sum(num)/np.sum(den1)
 
     w =np.logspace(-15, -7, 500, base=np.pi)
 
